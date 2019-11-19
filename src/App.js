e1d2d58309f283b8cc334aefbdd1e62c69cde046
Kael <khang.tran@senecacollege.ca>
2019-11-19 14:14:03 -0500

    add logo component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import { HomePage } from './pages';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/Shop/ShopPage';
+import Header from './components/Header/Header';
 
 function App() {
    return (
       <div>
-         <Route exact path='/' component={HomePage}/>
-         <Route path='/shop' component={ShopPage}/>
+         <Header/>
+         <Switch>
+            <Route exact path='/' component={HomePage}/>
+            <Route path='/shop' component={ShopPage}/>
+         </Switch>
       </div>
    );
 }
