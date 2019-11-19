b67244c6c617573b4c9289b1dbecddb4b0ce94eb
Kael <khang.tran@senecacollege.ca>
2019-11-19 14:03:30 -0500

    add collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { HomePage } from './pages';
 import { Route } from 'react-router-dom';
+import ShopPage from './pages/Shop/ShopPage';
 
 function App() {
    return (
       <div>
          <Route exact path='/' component={HomePage}/>
+         <Route path='/shop' component={ShopPage}/>
       </div>
    );
 }
--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -2,43 +2,10 @@ import React, { useState } from 'react';
 import MenuItem from '../MenuItem/MenuItem';
 
 import './styles.scss';
+import { SECTION_DATA } from '../../data';
 
 const Directory = () => {
-   const [sections, setSections] = useState([
-         {
-            title: 'hats',
-            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-            id: 1,
-            linkUrl: 'shop/hats'
-         },
-         {
-            title: 'jackets',
-            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-            id: 2,
-            linkUrl: 'shop/jackets'
-         },
-         {
-            title: 'sneakers',
-            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-            id: 3,
-            linkUrl: 'shop/sneakers'
-         },
-         {
-            title: 'womens',
-            imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-            size: 'large',
-            id: 4,
-            linkUrl: 'shop/womens'
-         },
-         {
-            title: 'mens',
-            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-            size: 'large',
-            id: 5,
-            linkUrl: 'shop/mens'
-         }
-      ]
-   );
+   const [sections] = useState(SECTION_DATA);
    return (
       <div className='directory-menu'>
          {sections.map(({ id, ...otherProps }) => (
